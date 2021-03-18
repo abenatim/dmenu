00188d7c02da0a1c50917631675dc71d5df8e83b
aabm <aabm@disroot.org>
2021-03-18 11:38:01 -0300

    Change font size, line num and line height

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Iosevka:size=11"
+	"Iosevka Medium:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,11 +14,11 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#dddddd", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 12;
+static unsigned int lines      = 9;
 
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 0;
-static unsigned int min_lineheight = 24;
+static unsigned int min_lineheight = 25;
 
 /*
  * Characters not considered part of a word while deleting words
